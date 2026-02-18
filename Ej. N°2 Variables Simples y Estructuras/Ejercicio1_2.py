a20cfc91b9f92010cd7bf36df73c1a28d5001615
Thiago Cristaldo <cristaldtiago@gmail.com>
2026-02-17 22:04:39 -0300

    Delete Ej. N°2 Variables Simples y Estructuras directory

--- Ej. N°2 Variables Simples y Estructuras/Ejercicio1_2.py
+++
@@ -1,9 +0,0 @@
-# 1.2-Escriba un programa que lea por teclado un valor de temperatura en grados Celsius y lo convierta a grados Fahrenheit.
-
-print("Conversor de grados Celsius (°C) a Fahrenheit (°F)")
-
-n = float(input("Ingrese grados en celsius: "))
-
-f = (n * (9 / 5)) + 32
-
-print(f"Son {f} grados fahrenheit")
--- Ej. N°2 Variables Simples y Estructuras/Ejercicio3_1.py
+++
@@ -1,9 +0,0 @@
-# 3.1-Escriba un programa en que lea un número entero n, calcule e imprima el factorial del mismo.
-
-n = int(input("Ingrese número: "))
-f = 1
-
-for i in range(1, n + 1):
-    f *= i
-
-print(f"{n}! es: {f}")
