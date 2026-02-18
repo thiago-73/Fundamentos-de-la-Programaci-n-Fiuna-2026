18db48179b44da98d49379a15973881b8232f478
Thiago Cristaldo <cristaldtiago@gmail.com>
2026-02-17 22:04:58 -0300

    Delete El. N°3 Estructuras de control directory

--- El. N°3 Estructuras de control/Elab1.py
+++
@@ -1,8 +0,0 @@
-# Elaborar un programa en que solicite al usuario que introduzca por teclado su edad, si esta es menor que 21 años que imprima en pantalla un mensaje que diga “Está prohibido que tomes bebidas alcohólicas”, en caso contrario imprimir en pantalla “Jaha a la BR a tomar”.
-
-edad = int(input("Ingrese edad: "))
-
-if edad < 21:
-    print("Está prohibido que tomes bebidas alcohólicas")
-else:
-    print("Jaha a la BR a tomar")
--- El. N°3 Estructuras de control/Elab3.py
+++
@@ -1,9 +0,0 @@
-# Elaborar un programa en que solicite al usuario que introduzca por teclado un número entero y positivo comprendido en el intervalo [1; 12] (en caso contrario finalizar el programa) e imprima en pantalla a que mes corresponde dicho número.
-
-numero = int(input("Ingrese un número del 1 al 12: "))
-meses = [0, "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
-
-if numero < 1 or numero > 12:
-    print("Valor ingresado incorrecto, el programa finalizará.")
-else:
-    print("El mes correspondiente es:", meses[numero])
